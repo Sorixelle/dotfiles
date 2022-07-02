dfe0bcd89a09949f290bfadb5b58a21d5d4483d6
Ruby Iris Juric <ruby@srxl.me>
2022-07-03 00:14:49 +1000

    home/theme/witchery: add colours to alacritty

--- home/themes/witchery/alacritty.nix
+++ home/themes/witchery/alacritty.nix
@@ -12,6 +12,32 @@
         normal.family = config.srxl.fonts.monospace.name;
         size = config.srxl.fonts.monospace.size;
       };
+      colors = {
+        primary = {
+          background = "#2A2522";
+          foreground = "#F3E4DF";
+        };
+        normal = {
+          black = "#2A2522";
+          red = "#7A5C55";
+          green = "#535D40";
+          yellow = "#A89159";
+          blue = "#47485B";
+          magenta = "#5B4187";
+          cyan = "#675391";
+          white = "#D8CAC6";
+        };
+        bright = {
+          black = "#5D4D4D";
+          red = "#A38678";
+          green = "#6E7C62";
+          yellow = "#D8BB73";
+          blue = "#6E708D";
+          magenta = "#835EC2";
+          cyan = "#917DB1";
+          white = "#F3E4DF";
+        };
+      };
     };
   };
 }
