f48ea02dd0d7d022a5bef92baecc4d5d4bc9e43b
Ruby Iris Juric <ruby@srxl.me>
2022-07-17 19:18:18 +1000

    theme/witchery: picom adjustments, move to separate module

---
+++ home/themes/witchery/picom.nix
@@ -0,0 +1,26 @@
+{ ... }:
+
+{
+  services.picom = {
+    enable = true;
+    experimentalBackends = true;
+    backend = "glx";
+    vSync = true;
+
+    fade = true;
+    fadeDelta = 4;
+
+    shadow = true;
+    shadowOffsets = [ (-10) (-10) ];
+    shadowOpacity = 0.7;
+
+    opacityRules = [ "90:class_g = 'Emacs'" ];
+
+    settings = {
+      blur = {
+        method = "dual_kawase";
+        strength = 5;
+      };
+    };
+  };
+}
