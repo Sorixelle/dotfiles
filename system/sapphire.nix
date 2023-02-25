6ff7dfe8fc2851f7cfa9f05d580645352a189d62
Ruby Iris Juric <ruby@srxl.me>
2023-02-25 15:27:59 +1100

    sapphire: Configure monitors in Xorg config

--- system/sapphire.nix
+++ system/sapphire.nix
@@ -206,6 +206,18 @@
           waitPID=$!
         '';
       }];
+
+      xrandrHeads = [
+        "HDMI-A-0"
+        {
+          output = "DisplayPort-0";
+          primary = true;
+          monitorConfig = ''
+            ModeLine "2560x1440@170" 718.40 2560 2608 2640 2720 1440 1443 1448 1554 +hsync -vsync
+            Option "PreferredMode" "2560x1440@170"
+          '';
+        }
+      ];
     };
   };
 
