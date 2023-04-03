e3859b4fc8e2005217130a3535ed9b4e00f4df72
Ruby Iris Juric <ruby@srxl.me>
2023-04-03 22:21:12 +1000

    Update nixpkgs

--- system/modules/audioprod.nix
+++ system/modules/audioprod.nix
@@ -26,48 +26,50 @@ in {
 
     users.users.${conf.user}.extraGroups = [ "audio" ];
 
-    services.pipewire.config = {
-      pipewire = {
-        "context.properties" = {
-          "default.clock.rate" = 48000;
-          "default.clock.allowed-rates" =
-            [ 44100 48000 88200 96000 176400 192000 352800 384000 ];
-          "default.clock.quantum" = 128;
-          "default.clock.min-quantum" = 32;
-          "default.clock.max-quantum" = 8192;
-        };
-        "context.modules" = [
+    environment.etc."pipewire/pipewire.conf.d/99-custom.conf".text = ''
+      {
+        "context.properties": {
+          "default.clock.rate": 48000,
+          "default.clock.allowed-rates": [ 44100, 48000, 88200, 96000, 176400, 192000, 352800, 384000 ],
+          "default.clock.quantum": 128,
+          "default.clock.min-quantum": 32,
+          "default.clock.max-quantum": 8192
+        },
+        "context.modules": [
           {
-            name = "libpipewire-module-rt";
-            args = {
-              "nice.level" = -15;
-              "rt.prio" = 88;
-              "rt.time.soft" = 200000;
-              "rt.time.hard" = 200000;
-            };
-            flags = [ "ifexists" "nofail" ];
-          }
-          { name = "libpipewire-module-protocol-native"; }
-          { name = "libpipewire-module-profiler"; }
-          { name = "libpipewire-module-metadata"; }
-          { name = "libpipewire-module-spa-device-factory"; }
-          { name = "libpipewire-module-spa-node-factory"; }
-          { name = "libpipewire-module-client-node"; }
-          { name = "libpipewire-module-client-device"; }
+            "name": "libpipewire-module-rt",
+            "args": {
+              "nice.level": -15,
+              "rt.prio": 88,
+              "rt.time.hard": 200000,
+              "rt.time.soft": 200000
+            },
+            "flags": [
+              "ifexists",
+              "nofail"
+            ]
+          },
+          { "name": "libpipewire-module-protocol-native" },
+          { "name": "libpipewire-module-profiler" },
+          { "name": "libpipewire-module-metadata" },
+          { "name": "libpipewire-module-spa-device-factory" },
+          { "name": "libpipewire-module-spa-node-factory" },
+          { "name": "libpipewire-module-client-node" },
+          { "name": "libpipewire-module-client-device" },
           {
-            name = "libpipewire-module-portal";
-            flags = [ "ifexists" "nofail" ];
-          }
+            "name": "libpipewire-module-portal",
+            "flags": [ "ifexists", "nofail" ]
+          },
           {
-            name = "libpipewire-module-access";
-            args = { };
-          }
-          { name = "libpipewire-module-adapter"; }
-          { name = "libpipewire-module-link-factory"; }
-          { name = "libpipewire-module-session-manager"; }
-        ];
-      };
-    };
+            "name": "libpipewire-module-access",
+            "args": {}
+          },
+          { "name": "libpipewire-module-adapter" },
+          { "name": "libpipewire-module-link-factory" },
+          { "name": "libpipewire-module-session-manager" }
+        ]
+      }
+    '';
 
     home-manager.users.${conf.user}.home.packages = with pkgs; [
       ardour
