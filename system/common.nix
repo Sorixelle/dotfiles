44a701215d8dac57439c56520ec2b438bdb49461
Ruby Iris Juric <ruby@srxl.me>
2023-06-07 18:53:51 +1000

    Rollback to Nix 2.15 due to regression

--- system/common.nix
+++ system/common.nix
@@ -22,7 +22,9 @@
   # Configure Nix
   nix = {
     # Use a flakes-enabled version of Nix
-    package = pkgs.nixUnstable;
+    # Known bug in 2.16 is breaking this config, revert when it gets fixed
+    # https://github.com/NixOS/nix/issues/8443
+    package = pkgs.nixVersions.nix_2_15;
 
     # Run automatic garbage collection at 4pm every Sunday
     gc = {
