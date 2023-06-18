f76e72f06de9c50acb115f50edc58e1b7d3903aa
Ruby Iris Juric <ruby@srxl.me>
2023-06-18 15:25:56 +1000

    Refine email config

--- home/sapphire-ruby/email.nix
+++ home/sapphire-ruby/email.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   programs.mbsync.enable = true;
@@ -6,15 +6,15 @@
   accounts.email = {
     maildirBasePath = "usr/mail";
     accounts = {
-      "ruby@srxl.me" = rec {
+      ruby-srxl = rec {
         address = "ruby@srxl.me";
         realName = "Ruby Iris Juric";
         gpg.key = "B6D7116C451A5B41";
         primary = true;
 
         userName = address;
-        # TODO: make this not suck balls
-        passwordCommand = "cat ~/.email-password";
+        passwordCommand =
+          "${pkgs.gnupg}/bin/gpg --quiet --decrypt --armor /home/ruby/.email-password";
         imap = {
           host = "imap.migadu.com";
           port = 993;
@@ -24,17 +24,49 @@
           port = 465;
         };
 
+        imapnotify = {
+          enable = true;
+          boxes = [
+            "Inbox"
+            "emacs-orgmode"
+            "help-gnu-emacs"
+            "info-gnu-emacs"
+            "mu-discuss"
+          ];
+          onNotify = "${pkgs.isync}/bin/mbsync ruby-srxl";
+          onNotifyPost = "${pkgs.mu}/bin/mu index";
+        };
+
         mbsync = {
           enable = true;
           create = "both";
+          expunge = "both";
         };
+        msmtp.enable = true;
         mu.enable = true;
       };
     };
   };
 
-  srxl.emacs.mu4e = {
-    enable = true;
-    address = "ruby@srxl.me";
+  programs.msmtp.enable = true;
+
+  services.imapnotify.enable = true;
+
+  srxl.emacs = {
+    mu4e = {
+      enable = true;
+      address = "ruby@srxl.me";
+    };
+
+    extraConfig = let smtp = config.accounts.email.accounts.ruby-srxl.smtp;
+    in ''
+      (setq mu4e-maildir-shortcuts
+            '((:maildir "/Inbox" :key ?i :name "Inbox"
+              (:maildir "/Receipts" :key ?r :name "Receipts" :hide-unread))))
+
+      (setq smtpmail-smtp-server "${smtp.host}"
+            smtpmail-smtp-service ${toString smtp.port}
+            smtpmail-stream-type 'ssl)
+    '';
   };
 }
