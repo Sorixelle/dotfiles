5913d2998fb8add7f6abc9a9a607bd662835832c
Ruby Iris Juric <ruby@srxl.me>
2024-05-23 12:24:29 +1000

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -78,10 +78,7 @@
 
     pre-commit-hooks = {
       url = "github:cachix/pre-commit-hooks.nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-utils.url = "github:numtide/flake-utils";
 
