d5b093b4dd808bb9701477f61a69085cc4fe4193
Ruby Iris Juric <ruby@srxl.me>
2024-05-26 18:33:39 +1000

    fbi-servefiles has a new repo

--- nixpkgs/packages/fbi-servefiles.nix
+++ nixpkgs/packages/fbi-servefiles.nix
@@ -1,14 +1,14 @@
 { stdenv, python3, fetchFromGitHub }:
 
-stdenv.mkDerivation rec {
+stdenv.mkDerivation {
   pname = "fbi-servefiles";
   version = "2.6.1";
 
   src = fetchFromGitHub {
-    owner = "Steveice10";
-    repo = "FBI";
-    rev = "ec259153e25fc77ee999b8caadac7e73d2e5e41a";
-    hash = "sha256-KUXAgiCF++JfpwpA3A0/2IGXdyRL2mUMJo4v4kMAJPE=";
+    owner = "nh-server";
+    repo = "FBI-NH";
+    rev = "a65971aec5c6e9b78daf955c53324de98f500fce";
+    hash = "sha256-c9sz06zv35kgJ+k+XaWV7KMe+nJOzro1iV52Pud1xXE=";
   };
   sourceRoot = "source/servefiles";
 
