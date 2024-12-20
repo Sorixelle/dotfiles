4d61dbbaeeca58f325c78d7c07b8abb469ad3454
Ruby Iris Juric <ruby@srxl.me>
2024-12-20 12:57:14 +1100

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     lix = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -103,7 +103,7 @@
           inputs.hyprlock.overlays.default
           inputs.hyprland-contrib.overlays.default
           #inputs.nixmox.overlay
-          inputs.nur.overlay
+          inputs.nur.overlays.default
           #inputs.rust-overlay.overlays.default
           #inputs.shadower.overlay
           #inputs.wired-notify.overlays.default
