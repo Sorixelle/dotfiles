13cc0620c1c3bb5f71fe8d0ee230b610b964662b
Ruby Iris Juric <ruby@srxl.me>
2025-03-22 16:18:38 +1100

    flake: update inputs, use lix betas

--- flake.nix
+++ flake.nix
@@ -4,8 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     lix = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
+      url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
+      flake = false;
+    };
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.lix.follows = "lix";
     };
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -103,7 +108,7 @@
           modules = [
             nixpkgs.nixosModules.notDetected
             inputs.home-manager.nixosModules.home-manager
-            inputs.lix.nixosModules.default
+            inputs.lix-module.nixosModules.default
             inputs.musnix.nixosModules.default
 
             (import ./system/common.nix)
@@ -132,7 +137,7 @@
       darwinConfigurations = {
         bauxite = nix-darwin.lib.darwinSystem {
           modules = [
-            inputs.lix.nixosModules.default
+            inputs.lix-module.nixosModules.default
             inputs.home-manager.darwinModules.home-manager
             { nixpkgs.pkgs = pkgsBySystem.aarch64-darwin; }
 
