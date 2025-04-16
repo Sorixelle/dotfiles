7b4dc4afe09cf2ef0d148087a792cebf7581bd17
Ruby Iris Juric <ruby@srxl.me>
2025-04-16 11:51:49 +1000

    modules[discord]: update pluralchum

--- home/modules/discord.nix
+++ home/modules/discord.nix
@@ -21,8 +21,8 @@
         "${catppuccin-theme}/themes/${themeFlavor}.theme.css";
 
       "BetterDiscord/plugins/Pluralchum.plugin.js".source = pkgs.fetchurl {
-        url = "https://github.com/estroBiologist/pluralchum/releases/download/2.5.0/Pluralchum.plugin.js";
-        hash = "sha256-5bvSQhYLIXMRAMb0jsrL0Rag2wNlORaQJj3LaN2JqCg=";
+        url = "https://github.com/estroBiologist/pluralchum/releases/download/2.6.0/Pluralchum.plugin.js";
+        hash = "sha256-R9OaVKXp0diyCVgk2hLLJYP3gb3VbN7zUVHpiIBCmyw=";
       };
     };
 }
