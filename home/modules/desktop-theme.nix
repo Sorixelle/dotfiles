cc3d5b7f9a817013770a8ed02f5680611f7a3e7c
Ruby Iris Juric <ruby@srxl.me>
2025-05-12 17:57:37 +1000

    modules[desktop-theme]: switch from unmaintained catppuccin theme to adw-gtk3 w/ catppuccin colors

--- home/modules/desktop-theme.nix
+++ home/modules/desktop-theme.nix
@@ -1,23 +1,39 @@
+{ config, pkgs, ... }:
+
+let
+  inherit (import ../../npins) adw-catppuccin;
+in
 {
-  # Get Catppuccin to style cursors, GTK and Qt apps
+  # Get Catppuccin to style cursors, icons and Qt apps using Kvantum
   catppuccin = {
     cursors.enable = true;
-    gtk = {
-      enable = true;
-      icon.enable = true;
-      tweaks = [ "rimless" ];
-    };
+    gtk.icon.enable = true;
     kvantum.enable = true;
   };
 
+  # Allow customisation of cursor icons in GTK apps and in the Sway desktop
   home.pointerCursor = {
     enable = true;
     gtk.enable = true;
     sway.enable = true;
   };
 
-  gtk.enable = true;
+  # Setup Catppuccin theming in GTK apps using adw-gtk3
+  gtk =
+    let
+      inherit (config.catppuccin) flavor accent;
+    in
+    {
+      enable = true;
+      theme = {
+        name = "adw-gtk3-dark";
+        package = pkgs.adw-gtk3;
+      };
+      gtk3.extraCss = builtins.readFile "${adw-catppuccin}/themes/${flavor}/catppuccin-${flavor}-${accent}.css";
+      gtk4.extraCss = builtins.readFile "${adw-catppuccin}/themes/${flavor}/catppuccin-${flavor}-${accent}.css";
+    };
 
+  # Setup Qt to use Kvantum styling
   qt = {
     enable = true;
     platformTheme.name = "kvantum";
